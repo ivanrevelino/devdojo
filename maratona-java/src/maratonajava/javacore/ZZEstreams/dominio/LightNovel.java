1913cf9ab6e6cac199595f8b0e6dd7b9c2b233a4
Ivan Revelino <234983236+ivanrevelino@users.noreply.github.com>
2026-03-11 18:20:07 +0200

    Streams 11/03

--- maratona-java/src/maratonajava/javacore/ZZEstreams/dominio/LightNovel.java
+++ maratona-java/src/maratonajava/javacore/ZZEstreams/dominio/LightNovel.java
@@ -1,5 +1,7 @@
 package maratonajava.javacore.ZZEstreams.dominio;
 
+import java.util.Objects;
+
 public class LightNovel {
 
     private String title;
@@ -24,5 +26,17 @@ public class LightNovel {
                 "title='" + title + '\'' +
                 ", price=" + price +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        LightNovel that = (LightNovel) o;
+        return Objects.equals(title, that.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(title);
     }
 }
--- maratona-java/src/maratonajava/javacore/ZZEstreams/test/StreamTest03.java
+++ maratona-java/src/maratonajava/javacore/ZZEstreams/test/StreamTest03.java
@@ -3,12 +3,10 @@ package maratonajava.javacore.ZZEstreams.test;
 import maratonajava.javacore.ZZEstreams.dominio.LightNovel;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-public class StreamTest02 {
-
+public class StreamTest03 {
     private static List<LightNovel> lightNovels= new ArrayList<>(List.of(
             new LightNovel("Tensei Shitarra", 8.99),
             new LightNovel("Over Lord", 3.99),
@@ -16,17 +14,24 @@ public class StreamTest02 {
             new LightNovel("No Game no life", 4.99),
             new LightNovel("Fullmetal Alchemist", 1.99),
             new LightNovel("Kumo desuga", 2.99),
+            new LightNovel("Kumo desuga", 2.99),
             new LightNovel("Monogatari", 8.99)))
             ;
 
     public static void main(String[] args) {
-        List<String> titles = lightNovels.stream()
-                .sorted(Comparator.comparing(LightNovel::getTitle))
-                .filter(ln -> ln.getPrice() <= 4)
-                .limit(3)
-                .map(LightNovel::getTitle)
-                .collect(Collectors.toList());
-        System.out.println(titles);
-    }
 
+
+        Stream<LightNovel> stream = lightNovels.stream();
+        lightNovels.forEach(System.out::println);
+
+        long count = stream
+                .distinct() // resumindo ele remove os itens repetidos baseados no equals e hashCode
+                .filter(ln -> ln.getPrice() < 4)
+                .count();
+
+        System.out.println(count);
+
+    }
 }
+
+
